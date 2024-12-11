3d95560d65bb006a5522ac1896e69075a2af04fb
T-hub5 <tarfafahad.03@gmail.com>
2024-12-11 23:29:25 +0000

    Update Suggested_recipes.py

--- Suggested_recipes.py
+++ Suggested_recipes.py
@@ -4,15 +4,16 @@ from groq import Groq
 
 ingredients = ["rice","chicken","tomato"]
 
-client = Groq(api_key="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")
-
-chat_completion = client.chat.completions.create(
+def suggest():
+    client = Groq(api_key="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")
+    chat_completion = client.chat.completions.create(
     messages=[
         {
             "role": "user",
             "content": f"go to {ingredients} and find one suitable recipes, give me the time taken, ingredients",
         }
     ],
-    model="llama3-8b-8192",
+    model="llama3-8b-81 92",
 )
-print(chat_completion.choices[0].message.content)
+return chat_completion.choices[0].message.content
+
