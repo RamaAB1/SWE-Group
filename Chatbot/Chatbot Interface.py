15c07335e2672c1985ee8d2bcf8c18ed8fb6647d
T-hub5 <tarfafahad.03@gmail.com>
2024-12-11 23:36:15 +0000

    Added the functions

--- Chatbot/Chatbot Interface.py
+++ Chatbot/Chatbot Interface.py
@@ -1,7 +1,12 @@
+from Manage import manage_ingredients, print_array
+from Base_code import list 
+from Suggested_recipe import suggest
+from Food_recipes import df 
+
 def chatbot():
     print("Welcome to the Recipe Suggestion Chatbot!")
     print("Please select an option:")
-    print("1. view ingredient list")
+    print("1. View ingredient list")
     print("2. View expired items")
     print("3. Suggest recipes")
     print("4. Chat with the Chat bot ")
@@ -11,21 +16,20 @@ def chatbot():
         choice = input("Enter your choice (1-5): ")
         if choice == "1":
             print(" Here are you ingredient list...")
-           # Call the function to display the ingredient list
+            print_array (list.manage_ingredients[0])
         elif choice == "2":
             print("Here are your expired items...")
-            # Call the function to suggest recipes
+            print_array (list.manage_ingredients[1])
         elif choice == "3":
             print("Here are some recipe suggestions...")
-            # Call the function to display the expired items
+            print (suggest)
         elif choice == "4":
             print("Chat with the Chat bot")
-            # Call the function to run the chatbot
+            chat(df)
         elif choice == "5":
             print ("Exit")
             break
         else:
             print("Invalid choice. Please try again.")
-
 
 chatbot()
