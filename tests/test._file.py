f893e40b5c9c4c0e52935fd4aeace18b0a437787
RamaAB1 <rxb235@student.bham.ac.uk>
2024-12-11 01:24:19 +0000

    Update test._file.py

--- tests/test._file.py
+++ tests/test._file.py
@@ -1,5 +1,19 @@
-from mypackage.main import main
+import unittest
+
+from VIEW_STORED_ITEMS_NOTIFICATIONS import main
 
 
-def test_main():
-    main()
+class TestSum(unittest.TestCase):
+    def test_list_output(self):
+        """
+        Test that it can sum a list of integers
+        """
+        result = main()
+        output = "Ingredient list:\Carrot 2025-1-1\n\n"
+        "Expired list:\nApple 2024-11-1\nBanana 2024-12-1\nEgg 2024-11-11\n"
+        "Flour 2024-11-24\nHoney 2024-11-30\nChocolate 2024-12-2\n"
+        
+        self.assertEqual(output, result)
+
+if __name__ == '__main__':
+    unittest.main()
