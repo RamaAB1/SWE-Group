033735e7c4f2169a7fdbdf0a749e80a897ff6977
RamaAB1 <rxb235@student.bham.ac.uk>
2024-12-12 00:29:14 +0000

    fixed a bug in Suggested recipes

--- Suggested_recipes.py
+++ Suggested_recipes.py
@@ -6,14 +6,20 @@ ingredients = ["rice","chicken","tomato"]
 
 def suggest():
     client = Groq(api_key="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")
-    chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
+
+    try:
+        chat_completion = client.chat.completions.create(
+            messages=[
+              
+                {
+                   "role": "user",
             "content": f"go to {ingredients} and find three suitable recipes, give me the time taken, ingredients",
-        }
-    ],
-    model="llama3-8b-81 92",
-)
-return chat_completion.choices[0].message.content
+                }
+            ],
+            model="llama3-8b-8192",
+        )
+        return chat_completion.choices[0].message.content
+    except Exception as e:
+        return f"An error occurred while using the Groq API: {e}"
+
 
