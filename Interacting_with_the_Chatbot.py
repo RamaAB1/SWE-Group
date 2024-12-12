01d1890ee97d0e066f358bf04e5bd0435f376e83
RamaAB1 <rxb235@student.bham.ac.uk>
2024-12-12 02:18:50 +0000

    Update Interacting_with_the_Chatbot.py

--- Interacting_with_the_Chatbot.py
+++ Interacting_with_the_Chatbot.py
@@ -1,32 +1,37 @@
+from Manage import list, print_array
+from Suggested_recipes import suggest
+
 def chatbot():
+
     print("Welcome to the Recipe Suggestion Chatbot!")
     print("Please select an option:")
-    print("1. view ingredient list")
+    print("1. View ingredient list")
     print("2. View expired items")
     print("3. Suggest recipes")
     print("4. Chat with the Chat bot ")
     print("5. Exit")
 
+    ingredients_list = list.manage_ingredients()
+
     while True:
         choice = input("Enter your choice (1-5): ")
         if choice == "1":
             print(" Here are you ingredient list...")
-           # Call the function to display the ingredient list
+            print_array (ingredients_list[0])
         elif choice == "2":
             print("Here are your expired items...")
-            # Call the function to suggest recipes
+            print_array (ingredients_list[1])
         elif choice == "3":
             print("Here are some recipe suggestions...")
-            # Call the function to display the expired items
+            print (suggest())
         elif choice == "4":
             print("Chat with the Chat bot")
-            # Call the function to run the chatbot
+            from Food_recipes import df , chat
+            chat(df)
         elif choice == "5":
             print ("Exit")
             break
         else:
             print("Invalid choice. Please try again.")
 
-# Run the chatbot
 chatbot()
-
